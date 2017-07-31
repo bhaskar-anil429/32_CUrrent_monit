e33db859dbde01e8643b7428d5c4d521a0ded0f3
Bhaskar <bhaskar.anil429@gmail.com>
2017-07-31 16:11:35 -0500

    Update 32C_Current_Monit.ino

--- 32C_Current_Monit.ino
+++ 32C_Current_Monit.ino
@@ -13,7 +13,7 @@ void setup()
 ///////////////////////////////////////////////////////////////////////////
 int read_current(int channel_number)
 {
-   if ( 0 < channel_number < 13)
+   if ( 0 < channel_number &&  channel_number < 13)
   {
     for ( byte i = channel_number; i<= channel_number; i++)
     { 
@@ -43,17 +43,18 @@ int read_current(int channel_number)
     Serial.print("Amp"); 
     Serial.print("\t");
     Serial.print("\n");
+//    Serial.print("I am here at 42"); 
     Wire.endTransmission(); // Complete Transmission
     }
 }
 
 //////////////////////////////////////
-  if ( 12 < channel_number < 25)
+  if ( 12 < channel_number && channel_number < 25)
   {
     for ( byte i = channel_number; i<= channel_number; i++)
     { 
       i = channel_number - 12;
-    Wire.beginTransmission(address+1); // Start communication
+    Wire.beginTransmission(43); // Start communication
     Wire.write(146); // Command header 
     Wire.write(106); // Command header 
     Wire.write(1);  // Command 1
@@ -63,7 +64,7 @@ int read_current(int channel_number)
     Wire.write(0); 
     Wire.write((146 + 106 + 1 + i + i + 0 + 0) & 255); // CheckSum
     Wire.endTransmission(); // Complete Transmission
-    Wire.requestFrom(address, 5); 
+    Wire.requestFrom(43, 5); 
     unsigned long MSB1 = Wire.read();
      MSB1= MSB1*65536;
     unsigned long MSB = Wire.read();
@@ -74,22 +75,23 @@ int read_current(int channel_number)
      Serial.print("new updates current values For channel   "); 
     Serial.print(channel_number);
     Serial.print("  ");
-    
     Serial.print(current,3);
     Serial.print("  ");
     Serial.print("Amp"); 
     Serial.print("\t");
     Serial.print("\n");
     Wire.endTransmission(); // Complete Transmission
+//    Serial.print("I am here at 43"); 
     }
-}
+  }
 //////////////////////////////////////////////////////////
-  if ( 24 < channel_number < 37)
+  if ( 24 < channel_number && channel_number < 37)
   {
     for ( byte i = channel_number; i<= channel_number; i++)
     { 
       i = channel_number - 24;
-    Wire.beginTransmission(address+2); // Start communication
+     
+    Wire.beginTransmission(44); // Start communication
     Wire.write(146); // Command header 
     Wire.write(106); // Command header 
     Wire.write(1);  // Command 1
@@ -99,7 +101,7 @@ int read_current(int channel_number)
     Wire.write(0); 
     Wire.write((146 + 106 + 1 + i + i + 0 + 0) & 255); // CheckSum
     Wire.endTransmission(); // Complete Transmission
-    Wire.requestFrom(address, 5); 
+    Wire.requestFrom(44, 5); 
     unsigned long MSB1 = Wire.read();
      MSB1= MSB1*65536;
     unsigned long MSB = Wire.read();
@@ -118,6 +120,8 @@ int read_current(int channel_number)
     Serial.print("\n");
    // delay(500);
     Wire.endTransmission(); // Complete Transmission
+//    Serial.print("I am here at 44"); 
+   
     }
 }
 //////////////////////////////////////////////////////////
@@ -125,6 +129,6 @@ int read_current(int channel_number)
 void loop()
 {
   ///////////////////////// enter the channel number///////////////
-  read_current(29);
-     delay(1000);
+  read_current(45);
+     delay(3000);
 }
